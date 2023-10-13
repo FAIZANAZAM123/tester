cf53dba714e4ab8ecf753b6d138f11f266c68fcb
FAIZANAZAM123 <103927526+FAIZANAZAM123@users.noreply.github.com>
2023-10-14 04:02:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $('.search-input').on('click', function() {
+//         $('#dropdownCard').toggle();
+//     });
+
+//     // Hide dropdown card when clicking outside of it
+//     $(document).on('click', function(event) {
+//         if (!$(event.target).closest('.search-form').length) {
+//             $('#dropdownCard').hide();
+//         }
+//     });
+// });
